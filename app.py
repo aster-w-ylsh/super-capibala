61f60545965684510faa7e9a380d6f38a33eb68d
aster-w-ylsh <aster.w.ylsh@gmail.com>
2025-04-07 13:41:27 +0800

    4.7.1

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>hello,world!>O<okk</p>"
+    return "<p>hello,world!>O<okk,sososososad</p>"
 
 if __name__=="__main__":
     app.run(debug=True)
